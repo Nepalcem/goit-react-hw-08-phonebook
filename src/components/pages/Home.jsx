721690da100b3c29332adbcbe6c1859be939b2d0
Nepalcem <mark.emailtest@gmail.com>
2023-07-01 18:11:23 +0300

    86%

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
 
 function Home() {
-  const location = useLocation();
-
   return (
     <div>
       <h1> Welcome to Phonebook application</h1>
--- src/components/routes/PrivateRoute.js
+++ src/components/routes/PrivateRoute.js
@@ -4,6 +4,7 @@ import { Navigate } from "react-router-dom";
 export const PrivateRoute = ({component: Component, redirectTo = "/"}) => {
     const {isLoggedIn, isRefreshing} = useAuthorize();
     const shouldRedirect = !isLoggedIn && !isRefreshing;
+    console.log('isLoggedIn:', isLoggedIn);
 
     return shouldRedirect ? <Navigate to={redirectTo}/> : <Component/>;
 }
