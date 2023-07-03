6991128ff8c43d7146659c1b2ea2e53af7831a8f
Nepalcem <mark.emailtest@gmail.com>
2023-07-03 22:34:08 +0300

    Update slice.js

--- src/redux/authorize/slice.js
+++ src/redux/authorize/slice.js
@@ -11,8 +11,8 @@ const AuthorizeSlice = createSlice({
   },
   extraReducers: builder => {
     builder
-      .addCase(register.pending, (state, action) => { })
-      .addCase(register.rejected, (state, action) => { })
+      .addCase(register.pending, (state, action) => {})
+      .addCase(register.rejected, (state, action) => {})
       .addCase(register.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
@@ -23,17 +23,19 @@ const AuthorizeSlice = createSlice({
         state.token = action.payload.token;
         state.isLoggedIn = true;
       })
-      .addCase(logIn.pending, (state, action) => { })
-      .addCase(logIn.rejected, (state, action) => { })
+      .addCase(logIn.pending, (state, action) => {})
+      .addCase(logIn.rejected, (state, action) => {})
       .addCase(logOut.fulfilled, (state, action) => {
         state.user = { name: null, email: null };
         state.token = null;
         state.isLoggedIn = false;
       })
+      .addCase(refreshUser.pending, (state, action) => {})
+      .addCase(refreshUser.rejected, (state, action) => {})
       .addCase(refreshUser.fulfilled, (state, action) => {
         state.user = action.payload;
         state.isLoggedIn = true;
-      })
+      });
   },
 });
 
