172c97354e0cd1a27c9b730f94de5e3e86526b79
Nepalcem <mark.emailtest@gmail.com>
2023-07-17 00:35:42 +0300

    margin-bottom

--- src/components/contactsList/ContactsList.styled.js
+++ src/components/contactsList/ContactsList.styled.js
@@ -12,7 +12,7 @@ export const ContactsStyled = styled.ul`
   & .item {
     width: 320px;
     padding: 6px 20px;
-    margin-bottom: 15px;
+    margin-bottom: 10px;
     border-radius: 4px;
     box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
     display: flex;
