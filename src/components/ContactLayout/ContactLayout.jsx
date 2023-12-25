6295e9d60052821cb0526467b7b0858b6b7d232d
Nepalcem <mark.emailtest@gmail.com>
2023-12-25 23:41:49 +0200

    Uni-Form

--- src/components/ContactLayout/ContactLayout.jsx
+++ src/components/ContactLayout/ContactLayout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Form from 'components/Form/Form';
+import Form from 'components/AddContactForm/Form';
 import Filter from 'components/Filter/Filter';
 import ContactsList from 'components/contactsList/ContactsList';
 import { ContentBlock } from './ContactLayout.styled';
